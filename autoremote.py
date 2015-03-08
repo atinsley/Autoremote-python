b6460328fbf18cdd0a99ceaf658349736516796e
atinsley <atinsley@gmail.com>
2015-03-08 11:29:34 -0400

    Various changes
    Added ability to use this script from the command line.  Also added HTML encoding of device name and message so they can be multiple words.  Tidied up settings section.

--- autoremote.py
+++ autoremote.py
@@ -1,22 +1,45 @@
 import re 
 import os
+import urllib
 import urllib2
-name='RaspiPi' # WHATEVER YOUR DEVICE NAME
+
+######################################
+#Change these settings
+######################################
+name='OpenHAB' # Whatever you want your device name to be
+localip='YOUR_LOCAL_IP' #Put your local static IP here
+key='YOUR_AUTOREMOTE_KEY' #Found by going to your custom Autoremote URL and then copying it out of the URL box
+
+#######################################
+#Do not modify below this line
+#######################################
+
+name = urllib.quote_plus(name)
 ipreq=urllib2.Request("http://ipecho.net/plain")
 ipres=urllib2.urlopen(ipreq)
 ipdata = ipres.read()
 publicip=ipdata
-localip='YOUR_LOCAL_IP'
-key='YOUR_AUTOREMOTE_KEY'
 
 def autoremote_reg():
-	req=urllib2.Request('http://autoremotejoaomgcd.appspot.com/registerpc?key={0}&name={1}&id=homepi&type=linux&publicip={2}&localip={3}'.format(key,name,publicip,localip))
+	req=urllib2.Request('http://autoremotejoaomgcd.appspot.com/registerpc?key={0}&name={1}&id=homepi&type=linux&publicip={2}&localip={3}&id={4}'.format(key,name,publicip,localip,name))
 	res=urllib2.urlopen(req)
 	data=res.read()
 
 def autoremote_send(message):
-	req=urllib2.Request('https://autoremotejoaomgcd.appspot.com/sendmessage?key={0}&message={1}'.format(key,message))
+	message = urllib.quote_plus(message)
+	req=urllib2.Request('https://autoremotejoaomgcd.appspot.com/sendmessage?key={0}&message={1}&sender={3}'.format(key,message,name))
 	res=urllib2.urlopen(req)
 	data=res.read()
 
-#autoremote_reg()
+if __name__ == "__main__":
+	import sys
+	#If there were no arguments supplied, we want to register the PC with Autoremote
+	if len(sys.argv) == 1:
+		autoremote_reg()
+		exit()
+		
+	#otherwise we want to send a message!
+	else:
+		message = sys.argv[1]
+		autoremote_send(message)
+		exit()
